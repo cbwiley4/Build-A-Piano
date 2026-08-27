30575008316215a6056d67e1d68c44c2de9716e0
caitlyn <cbwiley4@icloud.com>
2026-08-27 16:23:24 -0400

    lots

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+//vars
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+  // If the key is being held down, don't play the note again
+  if (e.repeat) {
+    return;
+  }
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
+
 // select all keys//
 const keys = document.querySelectorAll('.key')
 
@@ -10,5 +35,20 @@ keys.forEach((key) => {
 //handlers
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
+
+
+  //reset the notes current time to 0
+  noteAudio.currentTime = 0;
+  //play the note
   noteAudio.play();
+
+  //add a class, active, for styling purposes
+  key.classList.add('active');
+
+  //listen for when the sound has ended and remove active
+   noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
+
+ 
 }
